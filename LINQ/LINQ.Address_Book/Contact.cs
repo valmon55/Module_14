8aa3afbf52de690a307a80b590df859413ede0fd
valmon55 <fedor_ka@mail.ru>
2023-08-20 19:37:43 +0300

    Module 4 Task 3.3 start

---
+++ LINQ/LINQ.Address_Book/Contact.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ.Address_Book
+{
+    public class Contact
+    {
+        public Contact(string name, string lastName, long phoneNumber, String email) // метод-конструктор
+        {
+            Name = name;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+
+        public String Name { get; }
+        public String LastName { get; }
+        public long PhoneNumber { get; }
+        public String Email { get; }
+
+    }
+}
---
+++ LINQ/LINQ.Address_Book/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace LINQ.Address_Book
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //  создаём пустой список с типом данных Contact
+            var phoneBook = new List<Contact>();
+
+            // добавляем контакты
+            phoneBook.Add(new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Довлатов", 79990000010, "serge@example.com"));
+            phoneBook.Add(new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"));
+            phoneBook.Add(new Contact("Валерий", "Леонтьев", 79990000012, "valera@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Брин", 799900000013, "serg@example.com"));
+            phoneBook.Add(new Contact("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com"));
+
+            int pageNum;
+            while (true)
+            {
+                var key = Console.ReadKey().KeyChar;
+                var parsed = Int32.TryParse(key.ToString(), out pageNum);
+                Console.Clear();
+
+                IEnumerable<Contact> page = null;
+
+                if (!parsed || pageNum < 1 || pageNum > 3)
+                    Console.WriteLine("Page not found!");
+                else
+                    page = phoneBook.Skip((pageNum - 1) * 2).Take(2);
+
+
+                if (page != null)
+                    foreach (Contact contact in page)
+                        Console.WriteLine(contact.Name + " " +
+                                          contact.LastName + " - " +
+                                          contact.PhoneNumber.ToString() + " " +
+                                          contact.Email
+                                          );
+
+            }
+
+        }
+    }
+}
